604a91ea95102ec845dde303879ac826d415dd10
Alli <wonga10@gmail.com>
2019-03-28 13:28:05 -0700

    add set-verification tests

---
+++ SetTests/IsSetTests.cs
@@ -0,0 +1,45 @@
+using NUnit.Framework;
+using Set;
+
+namespace Tests
+{
+    public class IsSetTests
+    {
+
+        [Test]
+        public void TestAllMatching()
+        {
+            var card1 = new Card(Characteristic.Option1, Characteristic.Option1, Characteristic.Option1, Characteristic.Option1);
+            var card2 = new Card(Characteristic.Option1, Characteristic.Option1, Characteristic.Option1, Characteristic.Option1);
+            var card3 = new Card(Characteristic.Option1, Characteristic.Option1, Characteristic.Option1, Characteristic.Option1);
+            Assert.AreEqual(true, Card.IsSet(card1, card2, card3));
+        }
+
+        [Test]
+        public void TestNoneMatching()
+        {
+            var card1 = new Card(Characteristic.Option1, Characteristic.Option1, Characteristic.Option1, Characteristic.Option1);
+            var card2 = new Card(Characteristic.Option2, Characteristic.Option2, Characteristic.Option2, Characteristic.Option2);
+            var card3 = new Card(Characteristic.Option3, Characteristic.Option3, Characteristic.Option3, Characteristic.Option3);
+            Assert.AreEqual(true, Card.IsSet(card1, card2, card3));
+        }
+
+        [Test]
+        public void TestMismatchedSet()
+        {
+            var card1 = new Card(Characteristic.Option1, Characteristic.Option2, Characteristic.Option3, Characteristic.Option3);
+            var card2 = new Card(Characteristic.Option1, Characteristic.Option3, Characteristic.Option3, Characteristic.Option2);
+            var card3 = new Card(Characteristic.Option1, Characteristic.Option1, Characteristic.Option3, Characteristic.Option1);
+            Assert.AreEqual(true, Card.IsSet(card1, card2, card3));
+        }
+
+        [Test]
+        public void TestInvalidSet()
+        {
+            var card1 = new Card(Characteristic.Option2, Characteristic.Option2, Characteristic.Option3, Characteristic.Option3);
+            var card2 = new Card(Characteristic.Option1, Characteristic.Option3, Characteristic.Option3, Characteristic.Option2);
+            var card3 = new Card(Characteristic.Option1, Characteristic.Option1, Characteristic.Option3, Characteristic.Option1);
+            Assert.False(Card.IsSet(card1, card2, card3));
+        }
+    }
+}
