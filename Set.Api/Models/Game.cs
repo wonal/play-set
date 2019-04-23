b8be92952aa7cf35727433db3374ac3f3de4ed8c
Alli <wonga10@gmail.com>
2019-04-23 15:42:05 -0700

    board changes size depending on whether a set is present

--- Set.Api/Models/Game.cs
+++ Set.Api/Models/Game.cs
@@ -8,6 +8,7 @@ namespace SetApi.Models
         private Deck deck;
         public List<Card> Board { get; }
 
+
         public Game()
         {
             deck = new Deck();
@@ -20,13 +21,14 @@ namespace SetApi.Models
             }
         }
 
-        private static bool BoardContainsSet(List<Card> board)
+        public static bool BoardContainsSet(List<Card> board)
         {
-            for (int i = 0; i < 10; i++)
+            int length = board.Count;
+            for (int i = 0; i < length-2; i++)
             {
-                for(int j = i+1; j < 11; j++)
+                for(int j = i+1; j < length-1; j++)
                 {
-                    for (int k = j+1; k < 12; k++)
+                    for (int k = j+1; k < length; k++)
                     {
                         if (Game.IsSet(board[i], board[j], board[k]))
                         {
@@ -51,16 +53,48 @@ namespace SetApi.Models
         {
             if (Game.IsSet(card1, card2, card3)){
                 List<Card> cards = new List<Card> { card1, card2, card3 };
-                for (int i = 0; i < 3; i++)
+                if (Board.Count > 12)
                 {
-                    int index = Board.IndexOf(cards[i]);
-                    Board.RemoveAt(index);
-                    Board.Insert(index, deck.DrawCard(1)[0]);
+                    UpdateExtendedBoard(cards);
                 }
-                Board.AddRange(AddCardsIfNoSet(Board));
+                else
+                {
+                    UpdateNormalBoard(cards);
+                }
                 return (true, Board);
             }
             return (false, Board);
+        }
+
+        private void UpdateNormalBoard(List<Card> cards)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                int index = Board.IndexOf(cards[i]);
+                Board.RemoveAt(index);
+                Board.Insert(index, deck.DrawCard(1)[0]);
+            }
+            Board.AddRange(AddCardsIfNoSet(Board));
+  
+        }
+
+        private void UpdateExtendedBoard(List<Card> cards)
+        {
+            List<int> indices = new List<int>();
+            for (int i = 0; i < 3; i++)
+            {
+                indices.Add(Board.IndexOf(cards[i]));
+            }
+            cards.ForEach(card => Board.Remove(card));
+            List<Card> cardsToAdd = AddCardsIfNoSet(Board);
+            if (cardsToAdd.Count != 0)
+            {
+                indices.Sort();
+                for(int i = 0; i < 3; i++)
+                {
+                    Board.Insert(indices[i], cardsToAdd[i]);
+                }
+            }
         }
 
     }
