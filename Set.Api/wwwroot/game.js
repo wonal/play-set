026c74280dcbc8474030d10869a389a447081bfb
Alli <wonga10@gmail.com>
2019-05-13 15:53:00 -0700

    worked on styling
    rounded card edges, moved some game info to left of board

--- Set.Api/wwwroot/game.js
+++ Set.Api/wwwroot/game.js
@@ -33,7 +33,7 @@ export class Game {
     async createBoard() {
         const fetchData = {
             method: 'POST',
-            body: JSON.stringify({HasSeed: true, SeedValue: 100}), 
+            body: JSON.stringify({HasSeed: false, SeedValue: 100}),   //
             headers: new Headers({
                 'Content-Type': 'application/json',
                 'Accept-Encoding': 'application/json'
@@ -41,7 +41,7 @@ export class Game {
         };
         const response = await fetch(`${URL}/newgame`, fetchData);
         const data = await response.json();
-        this.seedMode = true;  
+        this.seedMode = false;   //
         this.gameID = data.gameID;
         this.topScores = data.topScores;
         this.board = [];
