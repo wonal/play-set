f2dfd9a961e0b7c9a1241b18c84740979fc93238
Alli <wonga10@gmail.com>
2019-06-22 07:45:32 -0700

    changed db health version, removed unnecessary startup code

--- Set.Api/Startup.cs
+++ Set.Api/Startup.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Set.Api;
-using SetApi.Models;
 
 namespace SetApi
 {
@@ -21,7 +19,6 @@ namespace SetApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            //services.AddDbContext<PlayerContext>();
             services.AddSingleton(Configuration);
             services.AddSingleton<Repository>();
         }
